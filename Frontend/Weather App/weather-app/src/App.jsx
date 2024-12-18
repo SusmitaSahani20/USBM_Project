7f9837b46f9856b607809733f6e35e51c55baca2
SusmitaSahani20 <susmitasahani05@gmail.com>
2024-12-18 11:25:36 +0530

    Weather App

---
+++ Frontend/Weather App/weather-app/src/App.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import Weather from './Components/Weather';
+
+
+
+const App = () => {
+  return (
+    <div className='app'>
+      <Weather/>
+    </div>
+  )
+}
+
+export default App
